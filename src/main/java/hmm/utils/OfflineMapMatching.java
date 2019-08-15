4aada847316e70f9944ac4ddb020e9512af6a403
joyce-ll <48090445+joyce-ll@users.noreply.github.com>
2019-08-15 10:52:51 +0800

    modify OfflineMapMatching

--- src/main/java/hmm/utils/OfflineMapMatching.java
+++ src/main/java/hmm/utils/OfflineMapMatching.java
@@ -7,6 +7,7 @@ import hmm.types.GpsMeasurement;
 import hmm.types.Point;
 import hmm.types.RoadPath;
 import hmm.types.RoadPosition;
+
 import java.util.*;
 
 
@@ -78,10 +79,23 @@ public class OfflineMapMatching {
      * For real map matching applications, one would compute the great circle distance between
      * two GPS points.
      */
-    private double computeDistance(Point p1, Point p2) {
-        final double xDiff = p1.x - p2.x;
-        final double yDiff = p1.y - p2.y;
-        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    private static double rad(double d) {
+        return d * Math.PI / 180.0;
+    }
+
+    public double computeDistance(Point p1, Point p2) {
+        double EARTH_RADIUS = 6378.137;
+        double radLat1 = rad(p1.latitude);
+        double radLat2 = rad(p2.latitude);
+        double a = radLat1 - radLat2;
+        double b = rad(p1.longitude) - rad(p2.longitude);
+        double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
+                + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+
+        s = s * EARTH_RADIUS;
+        s = Math.round(s * 10000d) / 10000d;
+        s = s * 1000;
+        return s;
     }
 
     /*
