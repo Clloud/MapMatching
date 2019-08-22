a248836582f77a20ba96e4870318a4bd2406dda5
Clloud <chenshaofengsf@163.com>
2019-08-22 20:22:06 +0800

    Revert "output roadIds"
    This reverts commit 7d8c8d1239a2cd636264d4d27301f0cb9d082436.

--- src/main/java/hmm/MapMatching.java
+++ src/main/java/hmm/MapMatching.java
@@ -1,37 +1,20 @@
 package hmm;
 
-import com.bmw.hmm.SequenceState;
 import hmm.reader.GpsDataReader;
 import hmm.reader.RoadDataReader;
-import hmm.storage.ToFille;
-import hmm.types.RoadPosition;
 import hmm.utils.OfflineMapMatching;
 
-import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class MapMatching {
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) {
         // prepare data
         List gpsMeasurements = GpsDataReader.getData("test_gps_data.txt");
         List roadEdges = RoadDataReader.getData("test_road_network.txt");
-        ToFille roadIdFile = new ToFille();
 
         // map matching
         OfflineMapMatching offlineMapMatching = new OfflineMapMatching(gpsMeasurements, roadEdges);
         List result = offlineMapMatching.run();
-        List<Long> roadIds = new ArrayList<>();
-        DataOutputStream dos = new DataOutputStream(new FileOutputStream("D:/HMM/road_ids_result.txt"));
-
-        for (Object o : result) {
-            long roadId = ((RoadPosition) ((SequenceState) o).state).edgeId;
-            if (roadIds.size() == 0 || roadId != roadIds.get(roadIds.size() - 1)) {
-                roadIds.add(roadId);
-                dos.writeLong(roadId);
-            }
-        }
-        dos.close();
-        System.out.println(roadIds);
+        System.out.println(result.size());
     }
 }
